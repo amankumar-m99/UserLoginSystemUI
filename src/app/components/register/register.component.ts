d3003fb2adedda105c147a4143e0ecf03f080e0a
AmanK <amankumar.m99@gmail.com>
2024-03-14 17:45:58 +0530

    wip registration process

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  step:number;
+  stepName:string;
+  accountDetails:FormGroup;
+  personalDetails:FormGroup;
+  securityDetails:FormGroup;
+  constructor(private formBuilder:FormBuilder){
+    this.step=1;
+    this.stepName = "AccountDetails";
+    this.accountDetails = formBuilder.group({
+      username: [''],
+      email: [''],
+      securityCode: [''],
+      password: [''],
+      confirmPassword: [''],
+    });
+    this.personalDetails = formBuilder.group({
+      firstName: [''],
+      middleName: [''],
+      lastName: [''],
+      gender: [''],
+      country: [''],
+      dateOfBirth: [''],
+    });
+    this.securityDetails = formBuilder.group({
+      loginAlert: [''],
+      passwordChangeAlert: [''],
+      twoStepLogin: [''],
+      promotionalMails: [''],
+    });
+  }
+  previous(){}
+  next(){
+    this.step++;
+    console.log(this.step);
+  }
+  submit(){}
 }
