19efa8d49e6cb4cbfffe524ebbcf6cf82a111bae
Aman Kumar <amankumar.m99@gmail.com>
2024-05-08 23:57:04 +0530

    deleted remains of cropperjs cleaned code added CDN for hammer js for pinch gesture

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,7 +3,6 @@ import { User } from 'src/app/model/user/user';
 import { UserService } from 'src/app/services/user/user.service';
 import { StaticData } from 'src/app/static/static-data';
 import { Utils } from 'src/app/utils/utils';
-import Cropper from 'cropperjs';
 import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -19,21 +18,19 @@ export class ProfileComponent implements OnInit, AfterViewInit{
   profilePicSrc = StaticData.apiBaseUrl+"/images/profile-pic/" + Utils.getCookie("userId");
   fallbackImageSrc = "../../../assets/images/profile_pic_green.png";
   fileName="";
-  uploadedImgSrc:any=""
-  cropper!:Cropper;
 
   imageChangedEvent: any = '';
   croppedImage: any = '';
   
   @ViewChild('profilePic') profilePic!:ElementRef;
   @ViewChild('fileUpload') inputFileElement!:ElementRef;
-  @ViewChild('uploadedImage') uploadedImage!:ElementRef;
   @ViewChild('profilePicPreviewModalButton') profilePicPreviewModalButton!:ElementRef;
 
   constructor(
     private userService:UserService,
     private sanitizer: DomSanitizer
   ){ }
+
   ngAfterViewInit(): void {
     this.profilePic.nativeElement.addEventListener("error", (e:any) => {
       this.profilePicSrc = this.fallbackImageSrc;
@@ -50,6 +47,37 @@ export class ProfileComponent implements OnInit, AfterViewInit{
       },
       complete: ()=>{}
     });
+  }
+
+  click():void{
+    this.inputFileElement.nativeElement.click();
+  }
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+    this.fileName = event.srcElement.files[0].name;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    let testStr:string = "";
+    if(typeof event.objectUrl == "string"){
+      testStr = event.objectUrl;
+    }
+    this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(testStr);
+    // event.blob can be used to upload the cropped image
+  }
+
+  imageLoaded(image: LoadedImage) {
+    // show cropper
+    this.profilePicPreviewModalButton.nativeElement.click();
+  }
+
+  cropperReady() {
+    // cropper ready
+  }
+
+  loadImageFailed() {
+    // show message
   }
 
   getName():string{
@@ -82,61 +110,5 @@ export class ProfileComponent implements OnInit, AfterViewInit{
     return roleName;
   }
 
-  onFileSelected(event: any):void{
-    const file:File = event.target.files[0];
-    if (file) {
-      this.profilePicPreviewModalButton.nativeElement.click();
-      this.uploadedImgSrc = file.webkitRelativePath+file.name;
-      this.fileName = this.uploadedImgSrc;
-      this.uploadedImgSrc = URL.createObjectURL(file);
-      // this.cropper = new Cropper(this.uploadedImage.nativeElement, {
-      //   zoomable:false,
-      //   scalable: false,
-      //   aspectRatio: 1,
-      //   crop: ()=>{
-      //     const canvas = this.cropper.getCroppedCanvas();
-      //     this.imageDestination = canvas.toDataURL("image/png");
-      //   }
-      // });
-      // const reader = new FileReader();
-      // reader.onload = e =>  this.uploadedImgSrc = reader.result;
-      // reader.readAsDataURL(file);
-      // this.userService.uploadPic(file).subscribe({
-      //   next: (response)=>{ alert(response.response);},
-      //   error: (error)=>{
-      //     console.log(error);
-      //   alert(error.error.message);
-      //   },
-      //   complete: ()=>{}
-      // });
-    }
-  }
-
-  click():void{
-    this.inputFileElement.nativeElement.click();
-  }
-
-  fileChangeEvent(event: any): void {
-    this.imageChangedEvent = event;
-}
-imageCropped(event: ImageCroppedEvent) {
-  let testStr:string = "";
-  if(typeof event.objectUrl == "string"){
-    testStr = event.objectUrl;
-  }
-  this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(testStr);
-  // event.blob can be used to upload the cropped image
-  console.log(event.blob);
-}
-imageLoaded(image: LoadedImage) {
-    // show cropper
-    this.profilePicPreviewModalButton.nativeElement.click();
-}
-cropperReady() {
-    // cropper ready
-}
-loadImageFailed() {
-    // show message
-}
 
 }
